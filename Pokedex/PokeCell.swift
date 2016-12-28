9191d5ea0c693999da9b34e04983a82d7346d77d
Christine Cunniff <christinecunniff@gmail.com>
2016-12-28 12:30:20 -0500

    Added and converted csv.swift file to Swift3.

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -13,7 +13,7 @@ class PokeCell: UICollectionViewCell {
         layer.cornerRadius = 5.0
     }
     
-    func configureCell(pokemon: Pokemon) {
+    func configureCell(_ pokemon: Pokemon) {
         self.pokemon = pokemon
         nameLbl.text = self.pokemon.name.capitalized
         thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
