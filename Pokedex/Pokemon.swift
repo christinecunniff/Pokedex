be8f3fac082b5291221355919e4a6ba326c84cb3
Christine Cunniff <christinecunniff@gmail.com>
2016-12-29 14:05:38 -0500

    Checked and hid each computed property.

--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -14,7 +14,54 @@ class Pokemon {
     private var _newxtEvolutionTxt: String!
     private var _pokemonURL: String!
     
+    var description: String {
+        if _description == nil {
+            _description = ""
+        }
+        return _description
+    }
     
+    var type: String {
+        if _type == nil {
+            _type = ""
+        }
+        return _type
+    }
+    
+    var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
+        return _defense
+    }
+    
+    var height: String {
+        if _height == nil {
+            _height = ""
+        }
+        return _height
+    }
+    
+    var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
+        return _weight
+    }
+    
+    var attack: String {
+        if _attack == nil {
+            _attack = ""
+        }
+        return _attack
+    }
+    
+    var nextEvolutionText: String {
+        if _newxtEvolutionTxt == nil {
+            _newxtEvolutionTxt = ""
+        }
+        return _newxtEvolutionTxt
+    }
     
     var name: String {
         return _name
